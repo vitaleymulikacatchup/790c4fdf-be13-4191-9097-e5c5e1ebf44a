e49b28d3855f04498430596eef51c43fb85c5315
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-09-24 11:39:20 +0300

    Update project files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
     images: ["/images/logo.svg"]
   },
   robots: { index: true, follow: true },
-  icons: [{ icon: "/favicon.ico" }]
+  icons: [{ rel: "icon", href: "/favicon.ico" }]
 };
 
 export default function RootLayout({
